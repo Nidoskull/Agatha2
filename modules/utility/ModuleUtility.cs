d592cadb16162f545cc79eade686665f5256ed7c
MistakeNot4892 <AGMogett@gmail.com>
2019-05-29 18:25:04 +1000

    Added impromptu voting listener.

--- modules/utility/ModuleUtility.cs
+++ modules/utility/ModuleUtility.cs
@@ -22,5 +22,18 @@ namespace Agatha2
 			commands.Add(new CommandDecide());
 			return true;
 		}
+		internal override void ListenTo(SocketMessage message, GuildConfig guild)
+		{
+			if(message.Content.Length < 200 && message.Content.Substring(message.Content.Length) == "?")
+			{
+				int firstOr = message.Content.FirstIndexOf(" or ");
+				int lastOr = message.Content.LastIndexOf(" or ");
+				if(firstOr > 0 && firstOr == lastOr)
+				{
+					message.AddReactionAsync(new Emoji("←"));
+					message.AddReactionAsync(new Emoji("→"));
+				}
+			}
+		}
 	}
 }
